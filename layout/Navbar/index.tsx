3b1cd756472592ed72671fbc3412eeeb624f17ae
Asilbek <asilbekeshtemirov@outlook.com>
2025-07-31 14:45:34 +0500

    created and finished layouts

--- layout/Navbar/index.tsx
+++ layout/Navbar/index.tsx
@@ -1,8 +1,24 @@
 import React from "react";
-import styles from "./navbar.module.css";
+import cls from "./navbar.module.css";
+import PlusIcon from "../../app/Profile/icons/plus.icon";
+import SettingsIcon from "../../app/Profile/icons/settings.icon";
+import UserIcon from "../../app/Profile/icons/user.icon";
+import Action from "@/components/actions";
 
 const NavbarLayout = () => {
-  return <div>NavbarLayout</div>;
+  return (
+    <div className="container">
+    <div className={cls.header}>
+      <div className={cls.title}>Code museum</div>
+
+      <div className={cls['header-actions']}>
+        <Action mode="light"><PlusIcon /></Action>
+        <Action mode="light"><SettingsIcon /></Action>
+        <Action mode="dark"><UserIcon /></Action>
+      </div>
+    </div>
+    </div>
+  );
 };
 
 export default NavbarLayout;
