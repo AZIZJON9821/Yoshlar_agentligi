25d6a2e37d364b88f132a0486eb58dc1afbe0ef5
Dilmuhammad <abdumalikovdilmuhammad06@gmail.com>
2025-07-31 14:40:33 +0500

    .

--- app/Home/index.tsx
+++ app/Home/index.tsx
@@ -1,10 +1,15 @@
 import CommentsModal from "@/components/Modal";
 import React from "react";
-
+import cls from "./home.module.css";
 const HomePage = () => {
-  return <div>HomePage
-    <CommentsModal/>
-  </div>;
+  return (
+    <div className="container">
+      <div className={cls["p"]}>
+        <p>Discover the coding world</p>
+      </div>
+      {/* <CommentsModal/> */}
+    </div>
+  );
 };
 
 export default HomePage;
