fab915949de7b04c2378688499e75fe48e03bd15
Dilmuhammad <abdumalikovdilmuhammad06@gmail.com>
2025-07-31 16:35:03 +0500

    merge

--- api/instances/codeMuseum.ts
+++ api/instances/codeMuseum.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "http://localhost:3000";
+export const baseURL = "https://api.it-mahalla.uz";
 export const customAxios = axios.create({
   baseURL: baseURL,
   withCredentials: true,
