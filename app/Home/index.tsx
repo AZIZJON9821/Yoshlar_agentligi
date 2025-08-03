cb7172a312a30e1a22e4251b8921d566d43e9009
Dilmuhammad <abdumalikovdilmuhammad06@gmail.com>
2025-08-03 23:02:47 +0500

    .

--- app/Home/index.tsx
+++ app/Home/index.tsx
@@ -5,7 +5,6 @@ import { PLang } from "@/components";
 import { useGetAllPosts } from "@/hook";
 import { customAxios } from "@/api";
 import { useAuth } from "@/context";
-import { getCookie } from "cookies-next";
 
 const HomePage = () => {
   const handleLike = (id: string | number) => {
@@ -17,7 +16,6 @@ const HomePage = () => {
     const data = { type: "dislike" };
     customAxios.post(`/posts/${id}/reactions`, data);
   };
-  console.log("token", getCookie("token"));
   const { selected } = useAuth();
 
   const { data: posts } = useGetAllPosts();
