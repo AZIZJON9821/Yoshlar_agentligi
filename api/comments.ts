213bf77a5d75c3188bd900d24a28151ba0099790
imd1lzod <dilzodbekxursanov0@gmail.com>
2025-08-04 18:50:16 +0500

    done

--- api/comments.ts
+++ api/comments.ts
@@ -11,7 +11,7 @@ export const leaveComment = async (payload: CommentProps) => {
   try {
   console.log(payload)
     const response = await customAxios.post(
-      `/posts/${payload.postId}/comment`,
+      `/posts/${payload.postId}/comments`,
       payload
     );
     return response.data;
@@ -35,3 +35,4 @@ export const getAllComments = async (postId: number | string) => {
     console.error("Error fetching comments:", error);
     return [];
   }
+}
