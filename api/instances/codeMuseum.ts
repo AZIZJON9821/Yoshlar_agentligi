39b7e0cdb0d4165ca66f629ff9126e51212789db
Dilmuhammad <abdumalikovdilmuhammad06@gmail.com>
2025-08-05 11:19:12 +0500

    .

--- api/instances/codeMuseum.ts
+++ api/instances/codeMuseum.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
-export const baseURL = "http://localhost:2006";
+export const baseURL = "https://api.it-mahalla.uz";
 
 export const customAxios = axios.create({
   baseURL: baseURL,
   withCredentials: true,
 });
+
